42e6694f854f881756237302c316391d7bea86b1
Martín <martinsps96@gmail.com>
2018-06-30 01:01:39 +0200

    Menú y primeros toques del player

---
+++ JuegoVerano/JuegoVerano/Assets/Scripts/PlayerScripts/MovePlayerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayerScript : MonoBehaviour {
+
+    bool jump = false;
+    public float moveSpeed = 15f;
+    public float jumpForce = 15f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+            jump = true;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("d"))
+        {
+            Rigidbody2D body = GetComponent<Rigidbody2D>();
+            //TODO SI está en el aire que se mueva menos (o agachao, o debuff...) 
+            body.velocity = new Vector2(moveSpeed, body.velocity.y);
+        }
+        if (Input.GetKey("a"))
+        {
+            Rigidbody2D body = GetComponent<Rigidbody2D>();
+            //TODO SI está en el aire que se mueva menos (o agachao, o debuff...)
+            body.velocity = new Vector2(-moveSpeed, body.velocity.y);
+        }
+        if (jump)
+        {
+            Rigidbody2D body = GetComponent<Rigidbody2D>();
+            //TODO Solo si está en el suelo (o si eso doble salto o algo (o una habilidad))
+            body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jump = false;
+        }
+        
+    }
+}
---
+++ JuegoVerano/JuegoVerano/Assets/Scripts/PlayerScripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public static PlayerScript instance;
+
+    //Singleton and for using it in other scenes
+    void Awake () {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+	}
+	
+
+}
